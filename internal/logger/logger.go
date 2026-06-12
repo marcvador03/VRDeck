602d474041cccd0162de3b677dc6fc4024a90e84
Marc Fleury <mfleury03@mailo.com>
2026-06-12 19:57:10 +0200

    [FEAT] improving logger adding Info / Error methods. Implemented prefix, moved logfile path to variable, and added Mutex

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,30 +3,42 @@ package logger
 import (
 	"fmt"
 	"os"
+	"sync"
 	"time"
 )
 
 type Logger struct {
-	file *os.File
+	file   *os.File
+	prefix string
+	mu     sync.Mutex
 }
 
 var logfile string = "streamdeckVR.log"
 
-func New() (*Logger, error) {
+func NewLogger(filepath, prefix string) (*Logger, error) {
 
-	file, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open log file: %w", err)
+		return nil, fmt.Errorf("failed to open log file %s: %w", filepath, err)
 	}
 
-	return &Logger{file: file}, nil
+	return &Logger{file: file, prefix: prefix}, nil
 }
 
 func (l *Logger) Log(message string) error {
-
-	timestamp := fmt.Sprintf("%s", time.Now().Format(time.RFC3339))
-	_, err := fmt.Fprintf(l.file, "%s %s\n", timestamp, message)
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	timestamp := time.Now().Format(time.RFC3339)
+	_, err := fmt.Fprintf(l.file, "%s %s%s\n", timestamp, l.prefix, message)
 	return err
+}
+
+func (l *Logger) Info(message string) error {
+	return l.Log(fmt.Sprintf("INFO: %s", message))
+}
+
+func (l *Logger) Error(err error) error {
+	return l.Log(fmt.Sprintf("ERROR: %v", err))
 }
 
 func (l *Logger) Close() error {
