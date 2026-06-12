acefbed4a726cd7c7c6ff4a34abe4dafd53f4d08
Marc Fleury <mfleury03@mailo.com>
2026-06-13 00:56:11 +0200

    [feat] implemented prototype for manifest name get and initial parse

--- internal/profiles/profiles.go
+++ internal/profiles/profiles.go
@@ -1,16 +1,65 @@
 package profiles
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	l "streamdeckVR/internal/logger"
 )
 
 type Profile struct {
-	UUID  string
-	name  string
-	pages int
+	UUID     string
+	name     string
+	pagesNum int
+	page     []Pages
 }
 
-func ProfileScan(path string) ([]os.DirEntry, error) {
+type Pages struct {
+	profile *Profile
+	UUID    string
+}
+
+type ProfileList struct {
+	profiles []Profile
+}
+
+func NewProfileList(path string, log *l.Logger) (*ProfileList, error) {
+	profilev3, err := os.ReadDir(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read path %s: %w", path, err)
+	}
+
+	for _, dir := range profilev3 {
+		if dir.IsDir() {
+			manifestPath := path + "\\" + dir.Name() + "\\" + "manifest.json"
+			var manifest map[string]interface{}
+			data, err := os.ReadFile(manifestPath)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+			if err := json.Unmarshal(data, &manifest); err != nil {
+				log.Error(err)
+			}
+			// Extract the "Name" field
+			if name, ok := manifest["Name"].(string); ok {
+				fmt.Printf("Name: %s\n", name)
+			} else {
+				fmt.Println("Field 'name' not found or not a string")
+			}
+		}
+		// fmt.Printf(
+		// 	"Name: %s,  : %v, Type: %v\n",
+		// 	dir.Name(),
+		// 	dir.IsDir(),
+		// 	dir.Type(),
+		// )
+		// fmt.Println(dir.Name())
+	}
+	return &ProfileList{}, nil
+}
+
+func profileListScan(path string) ([]os.DirEntry, error) {
 	files, err := os.ReadDir(path)
 
 	return files, err
--- main.go
+++ main.go
@@ -3,38 +3,28 @@ package main
 import (
 	"embed"
 	"fmt"
+	"os"
 
+	l "streamdeckVR/internal/logger"
 	p "streamdeckVR/internal/profiles"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
-type Profile struct {
-	UUID  string
-	name  string
-	pages int
-}
-
 func main() {
 	var path string
 	path = "C:\\Users\\mfleu\\AppData\\Roaming\\Elgato\\StreamDeck\\bProfilesV3"
-	files, err := p.ProfileScan(path)
-
+	log, err := l.NewLogger("./log.txt")
 	if err != nil {
-		fmt.Printf("Error reading directory: %v\n", err)
-		return
+		fmt.Fprintf(os.Stderr, "[Logger Error] %v\n", err)
+		os.Exit(1)
+	}
+	_, err = p.NewProfileList(path, log)
+	if err != nil {
+		log.Error(err)
 	}
 
-	// Iterate and print basic info
-	for _, entry := range files {
-		fmt.Printf(
-			"Name: %s, IsDir: %v, Type: %v\n",
-			entry.Name(),
-			entry.IsDir(),
-			entry.Type(),
-		)
-	}
 	// Create an instance of the app structure
 	// app := NewApp()
 
