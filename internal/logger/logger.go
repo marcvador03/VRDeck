6a9262c6355e91bc112d6e310388a6293e701519
Marc Fleury <mfleury03@mailo.com>
2026-06-26 17:04:17 +0200

    [feat] updated newlogger to zap

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,43 +3,33 @@ package logger
 import (
 	"fmt"
 	"os"
-	"sync"
-	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-type Logger struct {
-	file *os.File
-	mu   sync.Mutex
-}
-
-var logfile string = "streamdeckVR.log"
-
-func NewLogger(filepath string) (*Logger, error) {
+func NewLogger(filepath string) (*zap.Logger, error) {
 
 	file, err := os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open log file %s: %w", filepath, err)
 	}
+	config := zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		MessageKey:     "msg",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+	}
 
-	return &Logger{file: file}, nil
-}
-
-func (l *Logger) Log(message string) error {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	timestamp := time.Now().Format(time.RFC3339)
-	_, err := fmt.Fprintf(l.file, "%s %s\n", timestamp, message)
-	return err
-}
-
-func (l *Logger) Info(message string) error {
-	return l.Log(fmt.Sprintf("INFO: %s", message))
-}
-
-func (l *Logger) Error(err error) error {
-	return l.Log(fmt.Sprintf("ERROR: %v", err))
-}
-
-func (l *Logger) Close() error {
-	return l.file.Close()
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(config),
+		zapcore.AddSync(file),
+		zap.InfoLevel,
+	)
+	logger := zap.New(core, zap.AddCaller())
+	return logger, nil
 }
