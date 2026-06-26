3ae425215d246075de1132d6085395572be0c5c3
Marc Fleury <mfleury03@mailo.com>
2026-06-26 17:04:57 +0200

    [chore] updater path defiintion for profile, more idomatic

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	l "streamdeckVR/internal/logger"
 	p "streamdeckVR/internal/profiles"
@@ -29,13 +30,13 @@ func inspect_data(profileList p.ProfileList) {
 }
 
 func main() {
-	var path string
-	path = "C:\\Users\\mfleu\\AppData\\Roaming\\Elgato\\StreamDeck\\bProfilesV3"
-	log, err := l.NewLogger("./log.txt")
+	path := filepath.Join(os.Getenv("APPDATA"), "Elgato", "StreamDeck", "bProfilesV3")
+	log, err := l.NewLogger("log.txt")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "[Logger Error] %v\n", err)
 		os.Exit(1)
 	}
+	defer log.Sync()
 	ProfileList, err := p.NewProfileList(path, log)
 	if err != nil {
 		log.Error(err)
