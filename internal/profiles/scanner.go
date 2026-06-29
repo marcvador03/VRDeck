efd3a03597700d89e02dcec260053955388f02d5
Marc Fleury <mfleury03@mailo.com>
2026-06-29 14:42:42 +0200

    added retrials and sleep time to read manifest

--- internal/profiles/scanner.go
+++ internal/profiles/scanner.go
@@ -2,10 +2,12 @@ package profiles
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 	"streamdeckVR/internal/logger"
 	"strings"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 	"go.uber.org/zap"
@@ -28,28 +30,45 @@ func (p *ProfileList) sendPageUpdate(page *Pages) {
 	}
 }
 
+func (p *ProfileList) readCurrentManifest(manifestPath string, profileUUID string) ([]byte, error) {
+	log := logger.GetDefaultLogger()
+	for i := 0; i < 10; i++ {
+		data, err := os.ReadFile(manifestPath)
+		if err == nil {
+			return data, nil
+		}
+		if os.IsNotExist(err) || strings.Contains(err.Error(), "being used by another process") {
+			time.Sleep(200 * time.Millisecond)
+			continue
+		}
+		log.Error("Error while reading manifest file",
+			zap.String("UUID", profileUUID),
+			zap.Error(err))
+		return nil, err
+	}
+	log.Error("Error while reading manifest file after 10 trials",
+		zap.String("UUID", profileUUID))
+	return nil, fmt.Errorf("Error while reading manifest file after 10 trials")
+}
+
 func (p *ProfileList) getCurrentProfile(name string) (string, string, error) {
 	log := logger.GetDefaultLogger()
-	manifestPath := filepath.Join(name, "manifest.json")
-	profile := strings.TrimSuffix(filepath.Base(name), ".sdProfile")
+	profileUUID := strings.TrimSuffix(filepath.Base(name), ".sdProfile")
 	var page string
-	data, err := os.ReadFile(manifestPath)
+	data, err := p.readCurrentManifest(filepath.Join(name, "manifest.json"), profileUUID)
 	if err != nil {
-		log.Error("Error while reading manifest file",
-			zap.String("UUID", profile),
-			zap.Error(err))
-		return profile, page, err
+		return profileUUID, page, err
 	}
 	var current CurrentPage
 	err = json.Unmarshal([]byte(data), &current)
 	if err != nil {
 		log.Error(("Error while reading Json"),
-			zap.String("UUID", profile),
+			zap.String("UUID", profileUUID),
 			zap.Error(err))
-		return profile, page, err
+		return profileUUID, page, err
 	}
 	page = current.Pages.Current
-	return profile, page, nil
+	return profileUUID, page, nil
 }
 
 func (p *ProfileList) StartProfilesScan() {
